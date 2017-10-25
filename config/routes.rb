566684fdd9ffc7ec7bc1a7f3e227fb1de31de06c
Micheal Lane <mikeyd.lane@gmail.com>
2017-10-25 12:25:59 -0400

    nav test

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  root 'home#index'
   resources :comments
   resources :posts
   resources :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
